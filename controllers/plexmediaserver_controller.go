a0a440906251d84ceacdf55c5cb79d6ff03c96e5
Adam Kaplan <adam.kaplan@redhat.com>
2021-02-06 14:17:02 -0500

    Fix RBAC for controller
    - Add permission to manage services
    - Regenerate manifests

--- controllers/plexmediaserver_controller.go
+++ controllers/plexmediaserver_controller.go
@@ -32,6 +32,7 @@ type PlexMediaServerReconciler struct {
 // +kubebuilder:rbac:groups=plex.adambkaplan.com,resources=plexmediaservers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=plex.adambkaplan.com,resources=plexmediaservers/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
